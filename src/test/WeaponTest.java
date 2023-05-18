1d479a9557ad6d37634af4c9b38029c1acdd9743
nwebster02 <n8webster02@gmail.com>
2023-05-18 17:35:43 -0600

    small changes to WeaponTest to add more options

--- src/test/WeaponTest.java
+++ src/test/WeaponTest.java
@@ -16,7 +16,7 @@ public class WeaponTest {
 	final static int PROFICIENCY = 4;
 	final static int ABILITY_MOD = 6;
 
-	final static int AC = 0;
+	final static int AC = 22;
 
 	public static void main(String[] args) {
 
@@ -28,26 +28,32 @@ public class WeaponTest {
 		weaponList.add(new Weapon("Quill's Green-Flame Blade", "1d8+1d6+3 slashing + 2d8+4 fire"));
 		weaponList.add(new Weapon("Gunthar's Sharpshooter Crossbow w/ Bursting Bolts", "1d10+13 piercing + 2d6 force"));
 		weaponList.add(new Weapon("Ilzak's Smiting Lance of Vengeance", "1d12+7 piercing + 6d8 radiant"));
-		*/
 		weaponList.add(new Weapon("Qa'ri's Empowered Bite", "1d4+1d6+13 piercing + 2d8 psychic + 1d8 force"));
+		*/
 		weaponList.add(new Weapon("Eitri's Horrible Mess", "2d8+11 piercing + 4d8 radiant"));
 		
 		for (Weapon weapon : weaponList) {
 			DamageList damageList = new DamageList();
 			weaponDamages.put(weapon.getName(), damageList);
+			int numHit = 0;
 			for (int i = 0; i < ITERATION; i++) {
 				int roll = d20.roll();
+				//roll = Math.min(roll, d20.roll());
+				roll = Math.max(roll, d20.roll());
 				DamageList damages;
 				if (roll >= weapon.getCritPos()) {
 					damages = weapon.rollDamage(true);
-				} else if (roll > weapon.getCritNeg() && roll + ABILITY_MOD >= AC) {
+					numHit++;
+				} else if (roll > weapon.getCritNeg() && roll + ABILITY_MOD + PROFICIENCY >= AC) {
 					damages = weapon.rollDamage(false);
+					numHit++;
 				} else {
 					damages = new DamageList();
 				}
 				damageList.add(damages);
 			}
-			damageList.divide(ITERATION);
+			//damageList.divide(ITERATION);
+			damageList.divide(numHit);
 		}
 
 		System.out.println("Against a target with an AC of " + AC + ":");
